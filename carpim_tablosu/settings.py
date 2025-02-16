86d854ab42b7e7fc773d2f5d700b5bad247433ac
Bayram Güz <guzbayram@gmail.com>
2025-02-16 18:40:18 +0300

    PWA desteği aktif edildi

--- carpim_tablosu/settings.py
+++ carpim_tablosu/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    # "django_pwa",  # PWA desteği geçici olarak devre dışı
+    "django_pwa",  # PWA desteği aktif
 ]
 
 MIDDLEWARE = [
